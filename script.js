93281228766b5c0a844911296cf750b17d24d049
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-02-06 21:01:39 +0100

    1pushenLigne

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const slide = ["imgrec2/gr1.jpg", "imgrec2/gr2.jpg", "imgrec2/gr3.jpg", "imgrec2/gr4.jpg", "imgrec2/gr5.jpg"];
+const slide = [
+  "imgrec2/gr1.jpg",
+  "imgrec2/gr2.jpg",
+  "imgrec2/gr3.jpg",
+  "imgrec2/gr4.jpg",
+  "imgrec2/gr5.jpg",
+];
 let numero = 0;
 setInterval("ChangeSlide(1)", 4000);
 
-function ChangeSlide(sens) {
-    numero = numero + sens;
-    if (numero < 0)
-        numero = slide.length - 1;
-    if (numero > slide.length - 1)
-        numero = 0;
-    document.getElementById("slide").src = slide[numero];
-}
 const buttonPre = document.getElementById("precedent");
-buttonPre.addEventListener("click",()=>ChangeSlide(-1))
+buttonPre.addEventListener("click", () => ChangeSlide(-1));
 
 const buttonSuiv = document.getElementById("suivant");
-buttonSuiv.addEventListener("click",()=>ChangeSlide(1))
+buttonSuiv.addEventListener("click", () => ChangeSlide(1));
+
+function ChangeSlide(sens) {
+  numero = numero + sens;
+  if (numero < 0) numero = slide.length - 1;
+  if (numero > slide.length - 1) numero = 0;
+  document.getElementById("slide").src = slide[numero];
+}
+
+// FORMULAIRE
